5f25ccd31a4a1f35a7a27f2a227fbafeee96e2f8
jitendra <jitsingh182@gmail.com>
2023-01-25 21:17:36 +0530

    React Menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Menubar from "./Components/Menubar"
 import './Components/Stylemenu.css';
 
+
 function App() {
   return (
     <>
       <Menubar />
+
     </>
   );
 }
--- src/Components/Menubar.js
+++ src/Components/Menubar.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 
+
 function Menubar() {
   const sidebarRef = useRef(null);
   const sidebarBtnRef = useRef(null);
   const [menuData, setMenuData] = useState([]);
-  // const [isSubmenuOpen, setSubmenuOpen] = useState(false);
+
   const [activeMenuId, setActiveMenuId] = useState(false)
 
   const handleArrowClick = (e, id) => {
-    // e.stopPropogation();
-    // setSubmenuOpen(!isSubmenuOpen);
-    if (activeMenuId === false) {
+
+    if (activeMenuId === !id) {
 
       setActiveMenuId(id);
     } else {
@@ -21,7 +21,13 @@ function Menubar() {
 
   }
 
-  const handleSidebarBtnClick = (e) => {
+  const [itemName, setItemName] = useState()
+  const handleItemName = (e) => {
+    setItemName(e.target.innerHTML)
+    // console.log(itemName)
+  }
+
+  const handleSidebarBtnClick = () => {
 
     sidebarRef.current.classList.toggle("close");
   }
@@ -46,46 +52,55 @@ function Menubar() {
           <i className='bx bxl-react'></i>
           <span className="logo_name">Reacte Menu</span>
         </div>
-        <ul className="nav-links">{
-          menuData?.map(item => {
-            return <li key={item.category.id} onClick={(e) => handleArrowClick(e, item.category.id)} >
-              <div className="iocn-link">
-                <div className='cate-wrapper'>
+        <ul className="nav-links">
 
-                  <i><img src={item.category.ui_info.icon} alt="" /></i>
-                  <span className="link_name" >{item.category.name}</span>
 
+          {
+            menuData?.map(item => {
+              return <li key={item.category.id} onClick={(e) => handleArrowClick(e, item.category.id)} >
+                <div className="iocn-link">
+                  <div className='cate-wrapper'>
+
+                    <i><img src={item.category.ui_info.icon} alt="" /></i>
+                    <span className="link_name" onClick={handleItemName}>{item.category.name}</span>
+
+                  </div>
+                  <i className='bx bxs-chevron-down arrow' ></i>
                 </div>
-                <i className='bx bxs-chevron-down arrow' ></i>
-              </div>
-              <ul className={`sub-menu ${activeMenuId === item.category.id ? ' showMenu' : ''}`}>
-                <li><a className="link_name" href="#">{item.category.name}</a></li>
-                {item.sub_categories.map((subItem, index) => <li key={item.category.id + "sub_category_" + index}><a href={subItem.action_url}>{subItem.name}</a></li>)}
-              </ul>
-            </li>
+                <ul className={`sub-menu ${activeMenuId === item.category.id ? ' showMenu' : ''}`}>
+                  <li><a className="link_name" href="#">{item.category.name}</a></li>
+                  {item.sub_categories.map((subItem, index) => <li key={item.category.id + "sub_category" + index}><a href='#' onClick={handleItemName} >{subItem.name}</a ></li>)}
+                </ul>
+              </li>
 
-          })
-        }
-          <li>
-            <div className="profile-details">
-              <div className="profile-content">
-                <img src="https://media.discordapp.net/attachments/1064994063268462735/1065020319473082448/jitendrasingh_cyberpunk_neonkid_bk--v_4_bc389413-7dfa-4e09-b4fe-914b0d6a8428.png?width=555&height=555" alt="profileImg" />
-              </div>
-              <div className="name-job">
-                <div className="profile_name">Jitendra</div>
-                <div className="job">Web Devloper</div>
-              </div>
-              <i className='bx bx-log-out' ></i>
-            </div>
-          </li>
+            })
+          }
+
         </ul>
       </div>
       <section className="home-section" >
-        <div className="home-content">
-          <i className='bx bx-menu' ref={sidebarBtnRef} onClick={handleSidebarBtnClick} />
-          <span className="text">React JS Menubar</span>
+        <div className="nav">
+          <div className="home-content">
+            <i className='bx bx-menu' ref={sidebarBtnRef} onClick={handleSidebarBtnClick} />
+            <span className="text">React JS Menubar</span>
+
+          </div>
+          <div className="profile-details">
+            <div className="profile-content">
+              <img src="https://media.discordapp.net/attachments/1064994063268462735/1065020319473082448/jitendrasingh_cyberpunk_neonkid_bk--v_4_bc389413-7dfa-4e09-b4fe-914b0d6a8428.png?width=555&height=555" alt="profileImg" />
+            </div>
+            <div className="name-job">
+              <div className="profile_name">Jitendra</div>
+              <div className="job">Web Devloper</div>
+            </div>
+          </div>
         </div>
+        <div className="t">
+          {itemName}
+        </div>
+
       </section>
+
 
     </main>
   )
